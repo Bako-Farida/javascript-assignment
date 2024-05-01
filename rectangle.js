b102fffb1928bd817f1e374503e0d53fcf1c5892
Bako-Farida <bakofaridas@gmail.com>
2024-05-01 20:32:49 +0300

    calculated area

---
+++ rectangle.js
@@ -0,0 +1,21 @@
+// Function to Find the Area of Triangle
+function areaRectangle(length, width) {
+  let area = length * width;
+  return area;
+}
+
+// Function to Find the Parameter of Triangle
+function perimeterRectangle(length, width) {
+let perimeter = 2 * (length + width);
+return perimeter;
+}
+
+//input
+let length = 10;
+let width = 20;
+
+console.log("Area = " + areaRectangle(length, width));
+console.log("Perimeter = " + perimeterRectangle(length, width)); 
+
+
+
